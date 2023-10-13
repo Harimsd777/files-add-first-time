0254b249f21d59fc1d3f5dea081a1abb988f0d7b
Harimsd777 <140394780+Harimsd777@users.noreply.github.com>
2023-10-13 20:04:09 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+class Arithmetic
+  {
+     public static void main(String[]args)
+      {
+        float f1=65.98f;
+        float f2=44.02f;
+        double d3=f1+f2;
+        System.out.println(d3);
+
+        char ch1='a';
+        char ch2='b';
+        char i=ch1+ch2;
+        System.out.println(i);
+      }
+}
